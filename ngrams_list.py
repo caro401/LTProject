a90ed90941bf8691467dc2fd9b683a1b4ed13a50
caro401 <caroline.appleby@cantab.net>
2015-10-14 16:20:25 +0200

    initial tokenise function, unigram counting function completed

--- ngrams_list.py
+++ ngrams_list.py
@@ -7,6 +7,7 @@ class NGramModel:
     def __init__(self, file, n):
         self.file = file
         self.n = n
+        self.tokens = self.tokenise()
 
     def tokenise(self):
         """
@@ -14,16 +15,46 @@ class NGramModel:
         :return: linked list of tokens, including start/end of sentence tokens
         """
         # read in file
+        # TODO execption handling!
         with open(self.file) as fin:
             text = fin.read()
-            tokens_list = word_tokenize(text)  # list of tokens from NLTK
 
-        tokens_ll = linked_list.LinkedList
+            # tokenise the file using the NLTK word_tokenise() function
+            tokens_list = word_tokenize(text)
+
+        # put these tokens into a linked list
+        tokens_ll = linked_list.LinkedList()
         for tok in tokens_list:
             tokens_ll.list_insert_tail(tok)
 
-        # insert sentence start token at start of text
-        tokens_ll.list_insert_head("<s>")  # weeeeird
+        # insert sentence start token at start of text, sentence end token at end
+        tokens_ll.list_insert_head("<s>")
+        tokens_ll.list_insert_tail("</s>")
+
+        # search tokens_ll for instances of _Node with key ".", when find one, insert two nodes: </s> and <s>
+        x = tokens_ll._head
+        while x is not None:
+            if x.key == ".":  # when you find a full stop
+                tokens_ll.list_insert_middle(x, "</s>")  # insert end of sentence marker
+                tokens_ll.list_insert_middle(x.next_node, "<s>")  # and after that insert start of sentence marker
+            x = x.next_node  # look at next node
+
+        return tokens_ll
+
+    def unigram_count(self):
+        """
+        Work out the counts of all the unigrams in the list of tokens.
+        :return: dictionary of all unigrams and associated counts
+        """
+        unigram_dict = {}
+        x = self.tokens._head
+        while x is not None:
+            if x.key not in unigram_dict:  # if you haven't encountered this token yet
+                unigram_dict[x.key] = 1  # add a new entry to the dictionary, count 1
+            else:  # you have already seen this token
+                unigram_dict[x.key] += 1  # increment its count in the dictionary
+            x = x.next_node
+        return unigram_dict
 
 
 
@@ -31,6 +62,10 @@ class NGramModel:
 
 
 
-        # linked list (queue-like) to insert sentence boundary tokens
 
-        # return linked list
+
+
+
+if __name__ == "__main__":
+    mod = NGramModel("sml_test.txt", 2)
+    mod.tokenise()
